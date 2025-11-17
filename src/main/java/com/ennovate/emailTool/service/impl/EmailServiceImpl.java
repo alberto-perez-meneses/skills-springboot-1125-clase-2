b246557e5aa2a8f7ed1b7c6433b47a218f563354
Camilo <camilo.villarroel@dipreca.cl>
2025-11-17 08:37:02 -0300

    se cambia constructor por anotacion lombok

--- src/main/java/com/ennovate/emailTool/service/impl/EmailServiceImpl.java
+++ src/main/java/com/ennovate/emailTool/service/impl/EmailServiceImpl.java
@@ -2,19 +2,18 @@ package com.ennovate.emailTool.service.impl;
 
 import com.ennovate.emailTool.config.EmailValidator;
 import com.ennovate.emailTool.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @Service
 // todo: camilo cambiar constructor por anotacion de lombok
+@RequiredArgsConstructor
 public class EmailServiceImpl implements EmailService {
 
     private final EmailValidator emailValidator;
 
-    public EmailServiceImpl(EmailValidator emailValidator) {
-        this.emailValidator = emailValidator;
-    }
     // todo: jhonathan agregar comentarios con uso del metodo
     @Override
     public void addEmail(String email) {
